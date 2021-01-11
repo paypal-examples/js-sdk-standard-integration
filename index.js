04f2a718e489b90dd1862dcf842682ce418a3cdd
shrutikapoor08 <shruti.kapoor08@gmail.com>
2021-01-11 22:08:28 +0000

    Deploying to gh-pages from  @ 576ac31b84f32a5b482885dfbe71ba111165efdc 🚀

---
+++ index.js
@@ -0,0 +1,21 @@
+/* eslint-disable consistent-return, new-cap, no-alert, no-console */
+
+paypal.Buttons({
+    createOrder: function (data, actions) {
+        return actions.order.create({
+            purchase_units: [{
+                amount: {
+                    value: '50.00'
+                }
+            }]
+        });
+    },
+    onApprove: function (data, actions) {
+        return actions.order.capture().then(function (details) {
+            window.location.href = "approve.html";
+        });
+    },
+    onError: function (err) {
+        console.log('Something went wrong', err);
+    }
+}).render('#paypal-button-container'); // Display payment options on your web page
